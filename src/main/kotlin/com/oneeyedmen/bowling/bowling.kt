d17277bec1dedd0734b4cf4007484837c9579b78
Duncan McGregor <duncan@oneeyedmen.com>
2024-01-18 16:23:27 +0000

    Add tests and logic except for multi-player

--- src/main/kotlin/com/oneeyedmen/bowling/bowling.kt
+++ src/main/kotlin/com/oneeyedmen/bowling/bowling.kt
@@ -2,36 +2,69 @@ package com.oneeyedmen.bowling
 
 sealed interface Game {
     val lines: List<Line>
+    companion object {
+        operator fun invoke(vararg playerNames: String, frameCount: Int = 10): Game =
+            when {
+                playerNames.isEmpty() -> CompletedGame(emptyList())
+                else -> PlayableGame(*playerNames, frameCount = frameCount)
+            }
+    }
 }
 
 data class PlayableGame(override val lines: List<Line>) : Game {
+    private val currentLine: PlayableLine get() =
+        lines.singleOrNull() as? PlayableLine ?: TODO("handle more than one player")
+    val currentPlayer: String get() = currentLine.playerName.value
+
+    constructor(vararg playerNames: String, frameCount: Int) : this(
+        playerNames.map {
+            PlayableLine(
+                PlayerName(it),
+                List(frameCount) { UnplayedFrame() }
+            )
+        }
+    )
+
+    init {
+        require(lines.isNotEmpty())
+        require(lines.any { it is PlayableLine })
+    }
     fun roll(pinCount: PinCount): Game {
-        val currentLine: PlayableLine = TODO()
+        val currentLine: PlayableLine = this.currentLine
         val newLine: Line = currentLine.roll(pinCount)
-        val newLines: List<Line> = lines.replace(currentLine, newLine)
-        val completedLines: List<CompletedLine> = lines.filterIsInstance<CompletedLine>()
-        return if (completedLines.size < lines.size)
-            PlayableGame(newLines)
-        else
+        val newLines: List<Line> = lines.replacing(currentLine, newLine)
+        val completedLines: List<CompletedLine> = newLines.filterIsInstance<CompletedLine>()
+        return if (completedLines.size == lines.size)
             CompletedGame(completedLines)
+        else
+            PlayableGame(newLines)
     }
 }
-data class CompletedGame(override val lines: List<CompletedLine>) : Game
+data class CompletedGame(
+    override val lines: List<CompletedLine>
+) : Game
 
 sealed interface Line {
     val playerName: PlayerName
     val frames: List<Frame>
 }
-class PlayableLine(override val playerName: PlayerName, override val frames: List<Frame>) : Line {
+class PlayableLine(
+    override val playerName: PlayerName,
+    override val frames: List<Frame>
+) : Line {
+    init {
+        require(frames.isNotEmpty())
+        require(frames.any { it is PlayableFrame })
+    }
     fun roll(pinCount: PinCount): Line {
-        val currentFrame: PlayableFrame = TODO()
+        val currentFrame: PlayableFrame = frames.find { it is PlayableFrame } as PlayableFrame
         val newFrame: Frame = currentFrame.roll(pinCount)
-        val newFrames: List<Frame> = frames.replace(currentFrame, newFrame)
-        val completedFrames: List<CompletedFrame> = frames.filterIsInstance<CompletedFrame>()
-        return if (completedFrames.size < frames.size)
-            PlayableLine(playerName, newFrames)
-        else
+        val newFrames: List<Frame> = frames.replacing(currentFrame, newFrame)
+        val completedFrames = newFrames.filterIsInstance<CompletedFrame>()
+        return if (completedFrames.size == frames.size)
             CompletedLine(playerName, completedFrames)
+        else
+            PlayableLine(playerName, newFrames)
     }
 }
 
@@ -85,8 +118,8 @@ value class PinCount(val value: Int) {
     init { require(value in 0..10) }
 }
 
-private fun <E> List<E>.replace(currentLine: E, newLine: E): List<E> {
-    TODO("Not yet implemented")
+private fun <E> List<E>.replacing(item: E, newItem: E): List<E> =
+    map {if (it == item) newItem else it
 }
 
 class Player
--- src/test/kotlin/com/oneeyedmen/bowling/BowlingTests.kt
+++ src/test/kotlin/com/oneeyedmen/bowling/BowlingTests.kt
@@ -1,11 +1,58 @@
 package com.oneeyedmen.bowling
 
+import org.junit.jupiter.api.Disabled
 import org.junit.jupiter.api.Test
+import strikt.api.expectThat
+import strikt.assertions.isA
+import strikt.assertions.isEqualTo
+import kotlin.test.assertTrue
 
 class BowlingTests {
 
     @Test
-    fun test() {
+    fun `a game with no players isnt playable`() {
+        val game = Game()
+        expectThat(game).isA<CompletedGame>()
+    }
 
+    @Test
+    fun `a game with players is playable`() {
+        expectThat(Game("Fred")).isA<PlayableGame>()
+        expectThat(Game("Fred", "Barney")).isA<PlayableGame>()
+    }
+
+    @Disabled("WIP")
+    @Test
+    fun `play a game`() {
+        val game: Game = Game("Fred", "Barney")
+        assertTrue(game is PlayableGame)
+        expectThat(game.currentPlayer).isEqualTo("Fred")
+//        val game2: Game = game.roll(PinCount(1))
+//        assertTrue(game2 is PlayableGame)
+//        val game3: Game = game.roll(PinCount(1))
+    }
+
+    @Test
+    fun `play a game with just one player and one frame`() {
+        val game: Game = Game("Fred", frameCount = 1)
+        assertTrue(game is PlayableGame)
+        expectThat(game.currentPlayer).isEqualTo("Fred")
+        val game2: Game = game.roll(PinCount(1))
+        assertTrue(game2 is PlayableGame)
+        expectThat(game.currentPlayer).isEqualTo("Fred")
+        val game3: Game = game2.roll(PinCount(1))
+        assertTrue(game3 is CompletedGame)
+    }
+
+    @Test
+    fun `line`() {
+        val line = PlayableLine(
+            PlayerName("Fred"),
+            listOf(UnplayedFrame())
+        )
+        val line2 = line.roll(PinCount(1))
+        assertTrue(line2 is PlayableLine)
+        val line3 = line2.roll(PinCount(1))
+        assertTrue(line3 is CompletedLine)
     }
 }

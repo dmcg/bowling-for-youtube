57abe1f21d3c25ea08e65b89500a98bbd305570b
Duncan McGregor <duncan@oneeyedmen.com>
2024-03-08 14:28:24 +0000

    Tighten up tests

--- src/test/kotlin/com/oneeyedmen/bowling/BowlingPropertyTests.kt
+++ src/test/kotlin/com/oneeyedmen/bowling/BowlingPropertyTests.kt
@@ -1,6 +1,10 @@
 package com.oneeyedmen.bowling
 
-import net.jqwik.api.*
+import net.jqwik.api.Arbitrary
+import net.jqwik.api.ForAll
+import net.jqwik.api.Property
+import net.jqwik.api.Provide
+import net.jqwik.api.constraints.NotEmpty
 import net.jqwik.kotlin.api.any
 
 class BowlingPropertyTests {
@@ -8,50 +12,26 @@ class BowlingPropertyTests {
     @Property
     fun `a game with no players isnt playable`(
         @ForAll("nonNegativeIntegers") aNumberOfFrames: Int
-    ): Boolean {
-        return Game(
-            emptyList(),
-            frameCount = aNumberOfFrames
-        ) is CompletedGame
-    }
+    ) = Game(emptyList(), frameCount = aNumberOfFrames) is CompletedGame
 
     @Property
     fun `a game with no frames isnt playable`(
         @ForAll aListOfPlayerNames: List<String>
-    ): Boolean {
-        return Game(
-            aListOfPlayerNames,
-            frameCount = 0
-        ) is CompletedGame
-    }
+    ) = Game(aListOfPlayerNames, frameCount = 0) is CompletedGame
 
     @Property
     fun `cannot create a game with negative frame count`(
         @ForAll aListOfPlayerNames: List<String>,
         @ForAll("negativeIntegers") aNumberOfFrames: Int
-    ): Boolean {
-        return try {
-            Game(
-                aListOfPlayerNames,
-                frameCount = aNumberOfFrames
-            )
-            false
-        } catch (x: IllegalArgumentException) {
-            true
-        }
+    ) = failsWith<IllegalArgumentException> {
+        Game(aListOfPlayerNames, frameCount = aNumberOfFrames)
     }
 
     @Property
     fun `a game with players is playable`(
-        @ForAll aListOfPlayerNames: List<String>,
+        @ForAll @NotEmpty aListOfPlayerNames: List<String>,
         @ForAll("validFrameCount") aNumberOfFrames: Int
-    ): Boolean {
-        Assume.that(aListOfPlayerNames.isNotEmpty())
-        return Game(
-            aListOfPlayerNames,
-            aNumberOfFrames
-        ) is PlayableGame
-    }
+    ) = Game(aListOfPlayerNames, aNumberOfFrames) is PlayableGame
 
     @Provide
     fun nonNegativeIntegers(): Arbitrary<Int> = Int.any(0..Int.MAX_VALUE)
@@ -61,4 +41,11 @@ class BowlingPropertyTests {
 
     @Provide
     fun validFrameCount(): Arbitrary<Int> = Int.any(1..10)
+}
+
+inline fun <reified X : Throwable> failsWith(f: () -> Unit) = try {
+    f()
+    false
+} catch (x: Throwable) {
+    x is X
 }
